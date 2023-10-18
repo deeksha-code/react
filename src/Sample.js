eeed5ac96ca21e6370e716d9cd27857c544c3ab9
deeksha-code <deekshashetty736@gmail.com>
2023-10-18 11:14:15 +0530

    modified

---
+++ src/Sample.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./sample.css"
+
+function Sample() {
+    return (
+      <div className="main">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="heading">Hello my name is deeksha shetty</h1>
+      </div>
+    );
+}
+export default Sample;
